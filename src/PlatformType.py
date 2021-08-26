47cad58f8b7f75c733234851b67cbaf030cd1db1
Kim Hyeonseok <kim010322@gmail.com>
2021-08-26 15:04:09 +0900

    플랫폼 enum 추가, userscore 가져오기 기능 추가

---
+++ src/PlatformType.py
@@ -0,0 +1,28 @@
+from enum import Enum
+from enum import unique
+
+
+@unique
+class PlatformType(Enum):
+    PC = 'pc'
+    NS = 'switch'
+    WII_U = 'wii-u'
+    WII = 'wii'
+    GAMECUBE = 'gamecube'
+    N64 = 'nintendo-64'
+    N3DS = '3ds'
+    NDS = 'ds'
+    GBA = 'game-boy-advance'
+    PS5 = 'playstation-5'
+    PS4 = 'playstation-4'
+    PS3 = 'playstation-3'
+    PS2 = 'playstation-2'
+    PS = 'playstation'
+    PS_VITA = 'playstation-vita'
+    PSP = 'psp'
+    XSX = 'xbox-series-x'
+    XO = 'xbox-one'
+    XBOX = 'xbox'
+    STADIA = 'stadia'
+    IOS = 'ios'
+    DREAMCAST = 'dreamcast'
--- src/metacritic-scraper.py
+++ src/metacritic-scraper.py
@@ -3,20 +3,44 @@ from bs4 import BeautifulSoup
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-platformList = [
-    'pc',
-    'switch', 'wii-u', 'wii', 'gamecube', 'nintendo-64',
-    '3ds', 'ds', 'game-boy-advance',
-    'playstation-5', 'playstation-4', 'playstation-3', 'playstation-2', 'playstation',
-    'playstation-vita', 'psp',
-    'xbox-series-x', 'xbox-one', 'xbox',
-    'stadia', 'ios', 'dreamcast'
-]
+from PlatformType import PlatformType
 
 headers = {'User-Agent': 'Mozilla/5.0'}
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('https://', adapter)
+session.mount('https://', adapter)
 
 
-def get_rating_value(title: str) -> float:
+def get_metascore(title: str, platform: PlatformType) -> int or None:
+    if title is None:
+        raise TypeError('title must be str')
+
+    table = str.maketrans('', '', ":'")
+    modified_title = title.lower().translate(table).replace(' ', '-')
+
+    url = f"https://www.metacritic.com/game/{platform.value}/{modified_title}"
+
+    response = requests.get(url, headers=headers)
+
+    # 해당 플랫폼에 게임이 없는 경우
+    if not response.ok:
+        return None
+
+    html = response.text
+    soup = BeautifulSoup(html, features='html.parser')
+    element = soup.find('span', {'itemprop': 'ratingValue'})
+
+    # 아직 점수가 결정되지 않은 경우
+    if element is None:
+        return None
+
+    score = int(element.text)
+    return score
+
+
+def get_metascore(title: str) -> float:
     if title is None:
         raise TypeError('title must be str')
 
@@ -25,13 +49,8 @@ def get_rating_value(title: str) -> float:
 
     scores = []
 
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('https://', adapter)
-    session.mount('https://', adapter)
-
-    for platform in platformList:
+    for p in PlatformType:
+        platform = p.value
         url = f"https://www.metacritic.com/game/{platform}/{modified_title}"
 
         # response = requests.get(url, headers=headers)
@@ -56,3 +75,29 @@ def get_rating_value(title: str) -> float:
         raise ValueError(f"Can't find a game with name is {title}")
 
     return sum(scores) / len(scores)
+
+
+def get_user_score(title: str, platform: PlatformType):
+    if title is None:
+        raise TypeError('title must be str')
+
+    table = str.maketrans('', '', ":'")
+    modified_title = title.lower().translate(table).replace(' ', '-')
+    url = f"https://www.metacritic.com/game/{platform.value}/{modified_title}"
+
+    response = session.get(url, headers=headers)
+
+    # 해당 플랫폼에 게임이 없는 경우
+    if not response.ok:
+        return None
+
+    html = response.text
+    soup = BeautifulSoup(html, features='html.parser')
+    element = soup.select_one(
+        'div > div > div.left > div.with_trailer > div > div > div.summary_wrap > div.section.product_scores > div.details.side_details > div > div > a > div')
+    return float(element.text)
+
+
+if __name__ == '__main__':
+    tlou2 = 'the last of us part ii'
+    print(get_user_score(tlou2, PlatformType.PS4))
